cd00b2e3a4d4e187e5d1a511c5618b5fc3b8cd96
rishi <rishitanukala@gmail.com>
2025-07-14 00:37:33 +0530

    Adding Week4 Handson

---
+++ Week4/Module_7_Spring_REST_using_Spring_Boot3/Handson_6_Create_Authentication_Service_That_Returns_JWT/Code/spring_learn/security/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.cognizant.spring_learn.security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SecurityConfig.class);
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("admin").password(passwordEncoder().encode("pwd")).roles("ADMIN")
+            .and()
+            .withUser("user").password(passwordEncoder().encode("pwd")).roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().httpBasic().and()
+            .authorizeRequests()
+            .antMatchers("/authenticate").hasAnyRole("USER", "ADMIN")
+            .anyRequest().authenticated()
+            .and()
+            .addFilter(new JwtAuthorizationFilter(authenticationManager()));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        LOGGER.info("Start passwordEncoder()");
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
